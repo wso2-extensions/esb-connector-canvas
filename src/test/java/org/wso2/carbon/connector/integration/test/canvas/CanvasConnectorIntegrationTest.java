16b955853a2a383c4a6d9932ff6e5b9ad527d643
keetz <heykeetz@gmail.com>
2016-07-12 19:20:13 +0530

    Before the release

--- src/test/java/org/wso2/carbon/connector/integration/test/canvas/CanvasConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/canvas/CanvasConnectorIntegrationTest.java
@@ -49,7 +49,7 @@ public class CanvasConnectorIntegrationTest extends ConnectorIntegrationTestBase
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("canvas-connector-1.0.1-SNAPSHOT");
+        init("canvas-connector-1.0.1");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
